839d026a00da223f12ccecdf9be0a983020521b4
Beteab Admassu <beteab.admassu.tadele@gmail.com>
2022-05-13 20:08:38 +0300

    added index file and composer.json

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
+ $conn = new mysqli('remotemysql.com','NaXmWscQNV', '4mGy83U85M', 'NaXmWscQNV');
+ $sql = "SELECT * FROM `user`";
+ $result = mysqli_query($conn, $sql);
+ while($row=mysqli_fetch_array($result)){
+     ?>
+     <tr>
+         <td><?php echo $row['Fname'];?></td>
+         <td><?php echo $row['Lname'];?></td>
+     </tr>
+     <?php
+ }
 
- echo "Hello there Heroku";
 
 ?>
