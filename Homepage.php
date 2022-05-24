ba52d3edbe8fdffba42c762bc89417a94cd992f9
dagim19 <dagim.ashenafi.birru@gmail.com>
2022-05-24 22:31:45 +0300

    Home page is now dynamic

--- Homepage.php
+++ Homepage.php
@@ -1,3 +1,69 @@
+<?php
+
+    include_once 'backend/config/Database.php';
+    include_once 'backend/models/Course.php';
+    // Instantiate Database
+    $database = new Database();
+    $db = $database->connect();
+
+        $course = new Course($db);
+
+        $result = $course -> read();
+        $num = $result->rowCount();
+        // check for cookie
+        if(isset($_COOKIE['user'])) {
+            $user = $_COOKIE['user'];
+        }else {
+            $user = 'Guest';
+        }
+
+        //hash username and password
+        $user = hash('sha256', $user);
+
+        if($num > 0) {
+            // Post array
+            $courses_arr = array();
+            $courses_arr['allcourses'] = array();
+            $courses_arr['coursesenrolled'] = array();
+
+            while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                extract($row);
+
+                $course_item = array(
+                    'Id' => $ID,
+                    'Name' => $Name,
+                    'Instructor' => $Instructor,
+                    'Institute' => $Institute,
+                    'Description' => $Description,
+                    'Image' => $Image,
+                    'Preview_video_link' => $Preview_video_link,
+                );
+
+                //Push to "data"
+                array_push($courses_arr['allcourses'], $course_item);
+            }
+        // echo json_encode($courses_arr);
+        }
+        else {
+            echo json_encode(array('message' => 'No courses found'));
+        }
+
+
+
+
+    function renderVid($name, $instructor, $Preview_video_link) {
+        echo '<div class="vid">
+            <div class="videocontaint">
+            <iframe width="400" height="200" src='.$Preview_video_link.' title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <p>'.$name.'</p>
+            <p>Instructor: '.$instructor.'</p>
+            <p><a href="">Go to course</a></p>
+        </div>';
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -41,18 +107,25 @@
             </div>
         </div>
         <div class="videos">
-            <h1>Expand your career opportunities with Python</h1>
-            <p>Take one of Udemy’s range of Python courses and learn how to code using this incredibly useful language. Its simple syntax and readability makes Python perfect for Flask, Django, data science, and machine learning. You’ll learn how to build everything from games to sites to apps. Choose from a range of courses that will appeal to</p>
-            <button class='btn'>Explore Python</button>
+            <h1>Why Temari Dojo?</h1>
+            <p>Our online learning platform is filled with thousands of creative courses and classes taught by experts to help you learn new skills.</p>
+            <button class='btn'>Become a member</button>
             <div class="vid-box">
-                <div class="vid">
+                <!-- <div class="vid">
                     <div class="videocontaint">
                     <iframe width="400" height="200" src="https://www.youtube.com/embed/RsPWEmfOQdU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-                    <p>Instructor</p>
-                    <p>$344</p>
-                </div>
+                    <p>Instructor: </p>
+                    <p><a href="">Go to course</a></p>
+                </div> -->
+                <?php
+                    // iterate through the array of courses
+                    foreach($courses_arr['allcourses'] as $course) {
+                        renderVid($course['Name'], $course['Instructor'], $course['Preview_video_link']);
+                    }
+                    // renderVid('Introduction to golang', 'Fireship', 'https://www.youtube.com/embed/446E-r0rXHI');
+                ?>
                 <div class="vid">
                     <div class="videocontaint">
                         
@@ -61,7 +134,7 @@
                 <div class="vid">
                 <div class="videocontaint">
                         
-                        </div>
+                </div>
                 </div>
                 <div class="vid">
                 <div class="videocontaint">
