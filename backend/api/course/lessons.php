2886cf93a238429237ff846db06898bbff42a9ce
dagim19 <dagim.ashenafi.birru@gmail.com>
2022-05-31 10:56:44 +0300

    Lesson changed

--- backend/api/course/lessons.php
+++ backend/api/course/lessons.php
@@ -45,13 +45,46 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../../../css/Homepage.css">
     <link rel="stylesheet" href="../../../css/Lessons.css">
 </head>
 <body>
+    <nav class="nav">
+        <ul class="left">
+
+            <li><img src="./assets/image/chevron_up_29px.png" alt=""><a class='udemy' href="#">Temari Dojo</a></li>
+            <li><img src="./assets/image/search_24px.png" alt=""><input type="search" name="search" id="" class="same" placeholder='Search for anything'></li>
+
+            <li><img src="./assets/image/chevron_up_29px.png" alt=""><a class='udemy' href="Homepage.php">Temari Dojo</a></li>
+            <li class='search-form'>
+            <form action="backend/api/course/searchcourse.php" method="get">
+                <img src="./assets/image/search_24px.png" alt=""><input type="search" name="search" id="search" class="same" placeholder='Search for anything'>
+            </form>
+            </li>
+
+            
+        </ul>
+        <ul class="right">
+        <img class='menu' src="./assets/image/Menu_50px.png" alt="">
+            <div class="dropdown">
+
+
+                <button onclick="myFunction()" class="dropbtn" id="username">Username<i class="fa-solid fa-user usericon"></i> </button>
+                <div id="myDropdown" class="dropdown-content">
+                    <a class="myDropdown-a1" href="setting.php"><i class="fa-solid fa-user-pen usericon2"></i>Edit Profile</a>
+                    <a class="myDropdown-a1" href="Login.php"><i class="fa-solid fa-arrow-right-from-bracket usericon2"></i>Logout</a>
+
+                </div>
+            </div>
+        </ul>
+    </nav>
+    <div class="body">
+        
     <?php 
         foreach ($row as $value) {
             writeChapter($value['id'], $value['title'], $value['lessons']);
         }
     ?>
+    </div>
 </body>
 </html>
